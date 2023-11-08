007e05b3ed6f21e6811af09e5d184fffcc2c0143
Max <games.03@inbox.ru>
2023-11-08 19:55:11 +0300

    add WeatherService

--- WeatherServer/src/main/java/com/app/weatherserver/repository/WeatherRepository.java
+++ WeatherServer/src/main/java/com/app/weatherserver/repository/WeatherRepository.java
@@ -1,7 +1,16 @@
 package com.app.weatherserver.repository;
 
+import com.app.weatherserver.entity.Sensor;
 import com.app.weatherserver.entity.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.sql.Time;
+import java.util.List;
 
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
+    @Query("select w from Weather w where w.creator = ?1 order by w.time desc limit 20")
+    List<Weather> findWeatherFromSensor(Sensor key);
+    @Query("select w from Weather w where w.time >= ?1")
+    List<Weather> findCurrentWeatherFromSensor(Time time);
 }
