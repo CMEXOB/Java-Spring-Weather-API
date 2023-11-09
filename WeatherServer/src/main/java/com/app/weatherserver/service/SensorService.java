df0f4ad9b3993ea770fe9d52904f824f07e633ed
Max <games.03@inbox.ru>
2023-11-09 22:57:34 +0300

    add Sensor activity field

--- WeatherServer/src/main/java/com/app/weatherserver/service/SensorService.java
+++ WeatherServer/src/main/java/com/app/weatherserver/service/SensorService.java
@@ -29,6 +29,6 @@ public class SensorService {
     }
 
     public List<Sensor> getSensors() {
-        return sensorRepository.findAll();
+        return sensorRepository.findAllByIsActiveIsTrue();
     }
 }
