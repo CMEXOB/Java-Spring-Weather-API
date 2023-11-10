7cde81b7b939ae01e73ede90600b8b8fd73e24df
Max <games.03@inbox.ru>
2023-11-10 12:20:21 +0300

    add endpoints documentation

--- WeatherServer/src/main/java/com/app/weatherserver/controller/SensorController.java
+++ WeatherServer/src/main/java/com/app/weatherserver/controller/SensorController.java
@@ -1,9 +1,15 @@
 package com.app.weatherserver.controller;
 
+import com.app.weatherserver.dto.ApiErrorResponse;
 import com.app.weatherserver.dto.RegistrationRequest;
 import com.app.weatherserver.dto.RegistrationResponse;
 import com.app.weatherserver.entity.Sensor;
 import com.app.weatherserver.service.SensorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +26,23 @@ public class SensorController {
         this.sensorService = sensorService;
     }
 
+    @Operation(summary = "Register a sensor", description = "Registers a sensor with a unique name and returns UUID key")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful registration and UUID key provision"),
+            @ApiResponse(responseCode = "400", description = "Bad request - The sensor name not valid",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),
+            @ApiResponse(responseCode = "409", description = "Conflict - The sensor with given name already exist",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    })
     @PostMapping("registration")
     public ResponseEntity<RegistrationResponse> registerSensor(@Valid @RequestBody RegistrationRequest request){
-        RegistrationResponse response = (RegistrationResponse) sensorService.registerSensor(request.getName());
+        RegistrationResponse response = sensorService.registerSensor(request.getName());
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+    @Operation(summary = "Get active sensors", description = "Returns a sensors that which are active")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved")
+    })
     @GetMapping
     public List<Sensor> getSensors(){
         return sensorService.getSensors();
--- WeatherServer/src/main/java/com/app/weatherserver/controller/WeatherController.java
+++ WeatherServer/src/main/java/com/app/weatherserver/controller/WeatherController.java
@@ -1,8 +1,15 @@
 package com.app.weatherserver.controller;
 
+import com.app.weatherserver.dto.ApiErrorResponse;
 import com.app.weatherserver.dto.MeasureWeatherRequest;
 import com.app.weatherserver.entity.Weather;
 import com.app.weatherserver.service.WeatherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,15 +26,34 @@ public class WeatherController {
     public WeatherController(WeatherService weatherService) {
         this.weatherService = weatherService;
     }
+    @Operation(summary = "Save sensor weather measure", description = "Save weather measurements of sensor with given UUID key")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful registration and UUID key provision", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad request - The weather measurements data not valid",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),
+            @ApiResponse(responseCode = "409", description = "Conflict - The sensor with given name not exist",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    })
     @PostMapping("{key}/measurements")
-    public ResponseEntity measureWeatherFromSensor(@PathVariable UUID key, @Valid @RequestBody MeasureWeatherRequest measureWeatherRequest){
+    public ResponseEntity measureWeatherFromSensor(@Parameter(name = "key", description = "Sensor unique UUID", example = "a0fbb0de-62e5-4370-8bbe-a58242dc80a7") @PathVariable("key") UUID key,
+                                                   @Valid @RequestBody MeasureWeatherRequest measureWeatherRequest){
         weatherService.measureWeatherFromSensor(key, measureWeatherRequest.getValue(), measureWeatherRequest.getRaining());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+    @Operation(summary = "Get sensor weather measurements", description = "Returns 20 latest weather measurements of sensor with given UUID key")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "409", description = "Conflict - The sensor with given name not exist",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    })
     @GetMapping("{key}/measurements")
-    public List<Weather> getWeatherFromSensor(@PathVariable UUID key){
+    public List<Weather> getWeatherFromSensor(@Parameter(name = "key", description = "Sensor unique UUID", example = "a0fbb0de-62e5-4370-8bbe-a58242dc80a7") @PathVariable("key") UUID key){
         return weatherService.getWeatherFromSensor(key);
     }
+    @Operation(summary = "Get current weather measurements", description = "Returns weather measurements which were made no more than a minute ago")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved")
+    })
     @GetMapping("measurements")
     public List<Weather> getCurrentWeatherFromSensor(){
         return weatherService.getCurrentWeatherFromSensor();
