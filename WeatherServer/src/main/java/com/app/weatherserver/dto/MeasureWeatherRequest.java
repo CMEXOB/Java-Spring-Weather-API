6ff7c3150326a91ab2c36af0143443c35c184d98
Max <games.03@inbox.ru>
2023-11-10 20:45:57 +0300

    fix MeasureWeatherRequest error message

--- WeatherServer/src/main/java/com/app/weatherserver/dto/MeasureWeatherRequest.java
+++ WeatherServer/src/main/java/com/app/weatherserver/dto/MeasureWeatherRequest.java
@@ -16,7 +16,7 @@ public class MeasureWeatherRequest {
     /**
      * A value to represent temperature
      */
-    @NotNull(message = "Raining can't be empty")
+    @NotNull(message = "Value can't be empty")
     @Min(value = -100, message = "Temperature should not be less than -100")
     @Max(value = 100, message = "Temperature should not be greater than 100")
     private Double value;
